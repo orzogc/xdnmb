a0557c6d3c67e79f8331266ff4a59939d9fd2dca
orzogc <orzogc@163.com>
2023-05-09 17:03:16 +0800

    update dependencies

--- lib/app/data/models/reply.g.dart
+++ lib/app/data/models/reply.g.dart
@@ -7,7 +7,7 @@ part of 'reply.dart';
 // **************************************************************************
 
 // coverage:ignore-file
-// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters, always_specify_types
 
 extension GetReplyDataCollection on Isar {
   IsarCollection<ReplyData> get replyDatas => this.collection();
@@ -108,7 +108,7 @@ const ReplyDataSchema = CollectionSchema(
   getId: _replyDataGetId,
   getLinks: _replyDataGetLinks,
   attach: _replyDataAttach,
-  version: '3.0.5',
+  version: '3.1.0+1',
 );
 
 int _replyDataEstimateSize(
